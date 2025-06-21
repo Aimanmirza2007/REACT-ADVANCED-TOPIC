3d3445e982cc9666c1b869956453f95006b0028c
Aimanmirza2007 <sm501623@gmail.com>
2025-06-21 08:56:02 +0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React, { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+    <h1 className='bg-red-500'>Hello world</h1>
+    </>
+  )
+}
+
+export default App
